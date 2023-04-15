f6538bfee0ae58818b641adc79a7bf234da286b8
novitangrh <novitasabila70@gmail.com>
2023-04-16 01:08:30 +0700

    menambahkan API mahasiswa

---
+++ tpmodul9_1302213030/Controllers/MahasiswaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul9_1302213030.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> ListMhs = new List<Mahasiswa>
+        {
+            new Mahasiswa ("Novita Sabila Nugraha", "1302213030"),
+            new Mahasiswa ("Aisha Putri Nuryan", "1302213038"),
+            new Mahasiswa ("Jean Rika Haryadi", "1302210004"),
+            new Mahasiswa ("Jannatin Nurrohmah", "1302213051"),
+            new Mahasiswa ("Muthia Khairunnisa", "1302213062")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return ListMhs;
+        }
+
+        [HttpGet("{id}")]
+
+        public Mahasiswa Get(int index)
+        {
+            return ListMhs[index];
+        }
+
+        [HttpPost]
+
+        public void Post(Mahasiswa mahasiswa)
+        {
+            ListMhs.Add(mahasiswa);
+        }
+
+        [HttpDelete("{id}")]
+
+        public void Delete(int index)
+        {
+            ListMhs.RemoveAt(index);
+        }
+    }
+}
